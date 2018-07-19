d030f04c33868aeb497f0cc31d313df827d0d046
Elijah Smith <elijahs2106@gmail.com>
2018-07-19 18:37:09 -0400

    Add routes for landing page and airport page

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from 'express';
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('This will be the landing page.')
+    res.render('landing');
+});
+
+app.get('/airports', (req, res) => {
+    const airports = [
+        {name: 'Indianapolis International Airport (IND)', image: 'http://www.hok.com/uploads/2012/04/10/indy-airport01.jpg'},
+        {name: 'Amsterdam Schiphol International Airport (AMS)', image: 'https://amsterdamholland.ca/images/schiphollocation.jpg'},
+        {name: 'Boston Logan Internation Airport (BOS)', image: 'http://www.hok.com/uploads/2012/03/28/boston-logan01.jpg'},
+    ];
+
+    res.render('airports', { airports });
 });
 
 app.listen(3000, () => {
