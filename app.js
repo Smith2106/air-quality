4f31bedd595be04f692fde362e945bac7b928004
Elijah Smith <elijahs2106@gmail.com>
2018-07-19 22:14:37 -0400

    Move airports array and add route for adding new airport

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 
+const airports = [
+    {name: 'Indianapolis International Airport (IND)', image: 'http://www.hok.com/uploads/2012/04/10/indy-airport01.jpg'},
+    {name: 'Amsterdam Schiphol International Airport (AMS)', image: 'https://amsterdamholland.ca/images/schiphollocation.jpg'},
+    {name: 'Boston Logan Internation Airport (BOS)', image: 'http://www.hok.com/uploads/2012/03/28/boston-logan01.jpg'},
+];
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -8,13 +16,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/airports', (req, res) => {
-    const airports = [
-        {name: 'Indianapolis International Airport (IND)', image: 'http://www.hok.com/uploads/2012/04/10/indy-airport01.jpg'},
-        {name: 'Amsterdam Schiphol International Airport (AMS)', image: 'https://amsterdamholland.ca/images/schiphollocation.jpg'},
-        {name: 'Boston Logan Internation Airport (BOS)', image: 'http://www.hok.com/uploads/2012/03/28/boston-logan01.jpg'},
-    ];
-
     res.render('airports', { airports });
+});
+
+app.post('/airports', (req, res) => {
+    // Get data from form and add to airports array
+    const name = req.body.name;
+    const image = req.body.image;
+    const newAirport = {name, image};
+    airports.push(newAirport);
+    // Redirect to airports page
+    res.redirect('/airports');
+});
+
+app.get('/airports/new', (req, res) => {
+    res.render('new.ejs');
 });
 
 app.listen(3000, () => {
