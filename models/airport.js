fe1c04a8d296867e38d39d8ed15d02fde8fbac48
Elijah Smith <elijahs2106@gmail.com>
2018-07-21 15:15:50 -0400

    Add comments to the airport schema

--- models/airport.js
+++ models/airport.js
@@ -3,7 +3,13 @@ import mongoose from 'mongoose';
 const airportSchema = new mongoose.Schema({
     name: String,
     image: String,
-    description: String
+    description: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 });
 
 export default mongoose.model('Airport', airportSchema);
