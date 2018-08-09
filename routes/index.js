1759b97c77b12f71c2e8f3299513fb09150e8cda
Elijah Smith <elijahs2106@gmail.com>
2018-08-08 21:22:14 -0400

    Refactor index routes to seperate file

---
+++ routes/index.js
@@ -0,0 +1,61 @@
+import express from 'express';
+const router = express.Router();
+import passport from 'passport';
+
+import User from '../models/user';
+
+router.get('/', (req, res) => {
+    res.render('landing');
+});
+
+// ================
+// AUTH ROUTES
+// ================
+
+// Show register form
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// Handle sign up logic
+router.post('/register', (req, res) => {
+    const newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/airports');
+        });
+    });
+});
+
+// Show login form
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+// Handle sign in login
+router.post('/login', passport.authenticate('local', 
+    {
+        successRedirect: '/airports',
+        failureRedirect: '/login'
+    }), (req, res) => {
+});
+
+// Logout route
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/airports');
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+export default router;
