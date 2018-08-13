ace3637076cbcb94e647d194514b66d5ca96f3f6
Elijah Smith <elijahs2106@gmail.com>
2018-08-12 22:24:28 -0400

    Remove passing of hard coded error message to pages

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.post('/register', (req, res) => {
 
 // Show login form
 router.get('/login', (req, res) => {
-    res.render('login', { message: req.flash('error') });
+    res.render('login');
 })
 
 // Handle sign in login
@@ -49,6 +49,7 @@ router.post('/login', passport.authenticate('local',
 // Logout route
 router.get('/logout', (req, res) => {
     req.logout();
+    req.flash('success', 'Logged you out!');
     res.redirect('/airports');
 });
 
