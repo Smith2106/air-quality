4cbeaa9fe998cd851dbe606e40842657f73ac90d
Elijah Smith <elijahs2106@gmail.com>
2018-08-13 21:28:40 -0400

    Add created at data to airport schema

--- models/airport.js
+++ models/airport.js
@@ -5,6 +5,7 @@ const airportSchema = new mongoose.Schema({
     image: String,
     description: String,
     traffic: String,
+    createdAt: String,
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
