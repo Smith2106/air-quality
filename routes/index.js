65bd082bf43b4231a24ffa6b28948e04859376c3
Elijah Smith <elijahs2106@gmail.com>
2018-08-14 21:15:11 -0400

    Add page information to login and register routes

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 
 // Show register form
 router.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', {page: 'register'});
 });
 
 // Handle sign up logic
@@ -36,7 +36,7 @@ router.post('/register', (req, res) => {
 
 // Show login form
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {page: 'login'});
 })
 
 // Handle sign in login
