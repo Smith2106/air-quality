d8f82df36744bc7a02511041ad9c6184572d0fbe
Elijah Smith <elijahs2106@gmail.com>
2018-08-16 15:38:49 -0400

    Add avatar, firstName, lastName, and email to user schema

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ import passportLocalMongoose from 'passport-local-mongoose';
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avatar: {type: String, default: 'https://www.esparkinfo.com/wp-content/uploads/2016/08/default-avatar.png'},
+    firstName: String,
+    lastName: String,
+    email: String,
     isAdmin: {type: Boolean, default: false}
 });
 
